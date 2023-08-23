de3c8a6e6992c0ee1f15776caa8d8eaa9854b986
OmAR <omarsteven2398@gmail.com>
2023-08-23 22:41:12 +0300

    add file1

--- omar.sh
+++ omar.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-git add .
-git commit -m "$1"
-git push
+begin=1
+end=40
+
+for ((num=begin;num<=end;num++))
+do	
+	touch file1
+	git add .
+	git commit -m "add file1"
+	git push
+	rm file1
+	git add .
+	git commit -m "removed file1"
+	git push
+done
